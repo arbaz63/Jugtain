dfa8a34cd98be1ba91c92783f0366da41b273dde
arbaz63 <55198397+arbaz63@users.noreply.github.com>
2020-04-21 00:09:55 -0700

    updated admin js

--- src/jugat/Admin.js
+++ src/jugat/Admin.js
@@ -1,12 +1,12 @@
  //admin component
- import React,{useState} from 'react'
+ import React from 'react'
  import Jug from './Jug'
- import {fire,database} from '../config/Fire'
-import { updateLocale } from 'moment'
- const Admin=({cats,deleteCatagory,filteredJugat,deleteJugat,setJsearch,jSearch,updateCat})=>{
+ import {fire} from '../config/Fire'
+ const Admin=({cats,deleteCatagory,filteredJugat,deleteJugat,setJsearch,jSearch,updateCat,views})=>{
    
   return(
     <div>
+      <h2>{views}</h2>
       <input type="text"placeholder="Search jugat"value={jSearch}onChange={(e)=>setJsearch(e.target.value)}/>
       {/* catagories in txt form with delete btn */}
       {cats.map((cat,i)=>{
